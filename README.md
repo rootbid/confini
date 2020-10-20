6588c1a40bd00a1b6bb104197bf542aa43dd8a4d
Deepansh <34774585+rootbid@users.noreply.github.com>
2020-10-20 10:39:28 +0000

    Feature updates and bugs patched
    - Add configuration retrieval
    - Patch configuration related bugs
    - Fix project name
    - Update README

--- README.md
+++ README.md
@@ -1,31 +1,32 @@
 # Confini
- A cli application allowing secure and efficient storage, management of credentials (username & password) in a `conf.ini` file. 
-
-## TODO:
-- Enable retieval of credentials
-- Update crypto
-- Enhance scope of conf.ini file
+ A CLI application allowing secure and efficient storage, management of credentials (username & password) in a `conf.ini` file. 
+ Supported shells: Command Prompt, Powershell
 
 
 ## Usage
-1. Initiate a conf.ini file
+1. Initialize a `conf.ini` file
 ![configstore init](examples/init.gif)
 
-1. Initiate a conf.ini file
+2. Add a credential set to `conf.ini` file
 ![configstore add](examples/add.gif)
 
-1. Initiate a conf.ini file
+3. Edit/Update a credential set in a `conf.ini` file
 ![configstore edit](examples/edit.gif)
 
-1. Initiate a conf.ini file
+4. Remove a credential set from a `conf.ini` file
 ![configstore delete](examples/delete.gif)
 
+
+## TODO
+- [ ] Add passphrase verification
+- [ ] Enable support for linux shells
+- [ ] Update crypto
 
 ## Contribute
 There are many ways in which you can participate in the project, for example:
 
 - Submit bugs and feature requests, and help me verify as they are checked in
 - Review source code changes
-- Review the documentation and make pull requests for anything from typos to new content
+- Review the documentation
 
 If you are interested in fixing issues and contributing directly to the code base, please feel free to raise a PR.
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ extern crate rand_core;
 use self::base64::{decode, encode};
 use clap::{App, Arg};
 use crypto::aes::{self, KeySize};
-use crypto::symmetriccipher::SynchronousStreamCipher;
+use crypto::buffer::{self, BufferResult, ReadBuffer, WriteBuffer};
+use crypto::symmetriccipher::{Decryptor, SynchronousStreamCipher};
 use ini::Ini;
 use rand_core::{OsRng, RngCore};
 use std::env;
 use std::iter::repeat;
 use std::process::{Command, Stdio};
+
+const FILEPATH: &str = "C:\\.conf.ini";
 
 fn main() {
     let matches = App::new("Configuration Management")
@@ -25,7 +28,7 @@ fn main() {
                 .arg(
                     Arg::with_name("domain")
                         .short("d")
-                        .help("'Technology-Vendor' name for the domain to add")
+                        .help("Name for the configuration domain to add")
                         .takes_value(true)
                         .required(true),
                 )
@@ -50,7 +53,7 @@ fn main() {
                 .arg(
                     Arg::with_name("domain")
                         .short("d")
-                        .help("'Technology-Vendor' name for the domain to be modified")
+                        .help("Name for the configuration domain to be modified")
                         .takes_value(true)
                         .required(true),
                 )
@@ -74,15 +77,27 @@ fn main() {
                 .arg(
                     Arg::with_name("domain")
                         .short("d")
-                        .help("'Technology-Vendor' name for the domain to be modified")
+                        .help("Name for the configuration domain to be modified")
                         .takes_value(true)
                         .required(true),
-                )
+                ),
+        )
+        .subcommand(
+            App::new("retrieve")
+                .about("Retrive a configuration section")
+                .arg(
+                    Arg::with_name("domain")
+                        .short("d")
+                        .help("Name for the configuration domain to be modified")
+                        .takes_value(true)
+                        .required(true),
+                ),
         )
         .get_matches();
 
     if matches.is_present("init") {
-        // Generate a secret key and a nounce value with a secure random generator
+        // Generate a secret key and a nounce value with
+        // a secure random generator
         let mut key: Vec<u8> = repeat(0u8).take(16).collect();
         OsRng.fill_bytes(&mut key[..]);
         let mut nounce: Vec<u8> = repeat(0u8).take(16).collect();
@@ -121,17 +136,40 @@ fn main() {
         // Initialize a template for `conf.ini`
         let mut conf = Ini::new();
         conf.with_general_section().set("encoding", "utf-8");
-        conf.write_to_file("conf.ini").unwrap();
-        println!("Configuration file 'conf.ini' initiated");
-        println!("Use `configstore.exe add <domain> <username> <password>` to add configurations")
+        conf.write_to_file(FILEPATH).unwrap();
+        println!("Configuration file initialized at: {}", FILEPATH);
+        println!("Use `configstore.exe add <domain> <username> <password>` \
+            to add configurations")
     } else {
         match matches.subcommand() {
             ("add", Some(add_matches)) => {
-                let mut conf = Ini::load_from_file("conf.ini").unwrap();
-                let _key = "C_KEY";
-                let _nounce = "C_NOUNCE";
-                let key = decode(env::var(_key).unwrap()).unwrap();
-                let nounce = decode(env::var(_nounce).unwrap()).unwrap();
+                let mut conf = Ini::load_from_file(FILEPATH).unwrap();
+                let _key = match env::var("C_KEY") {
+                    Ok(k) => k,
+                    Err(e) => {
+                        println!(
+                            "Essential ENV variables could not be read\n \
+                        Try running the shell with elevated priviliges or \
+                        restarting the shell."
+                        );
+                        eprintln!("Error: C_KEY {:?}", e);
+                        std::process::exit(1)
+                    }
+                };
+                let _nounce = match env::var("C_NOUNCE") {
+                    Ok(k) => k,
+                    Err(e) => {
+                        println!(
+                            "Essential ENV variables could not be read\n \
+                        Try running the shell with elevated priviliges or \
+                        restarting the shell."
+                        );
+                        eprintln!("Error: C_NOUNCE {:?}", e);
+                        std::process::exit(1)
+                    }
+                };
+                let key = decode(_key).unwrap();
+                let nounce = decode(_nounce).unwrap();
 
                 let mut cipher = aes::ctr(KeySize::KeySize128, &key, &nounce);
                 let mut password: Vec<u8> = repeat(0u8)
@@ -142,17 +180,29 @@ fn main() {
                     &mut password[..],
                 );
 
-                conf.with_section(add_matches.value_of("domain"))
-                    .set("username", add_matches.value_of("username").unwrap())
-                    .set("password", encode(&password).as_str());
-                conf.write_to_file("conf.ini").unwrap();
-                println!(
-                    "Configuration for {} has been added",
-                    add_matches.value_of("domain").unwrap()
-                );
+                match conf.section(add_matches.value_of("domain")) {
+                    None => {
+                        conf.with_section(add_matches.value_of("domain"))
+                            .set("username", add_matches.value_of("username").unwrap())
+                            .set("password", encode(&password).as_str());
+                        conf.write_to_file(FILEPATH).unwrap();
+                        println!(
+                            "Configuration for {} has been added",
+                            add_matches.value_of("domain").unwrap()
+                        );
+                    }
+                    Some(d) => {
+                        println!(
+                            "Configuration already exists for {} with username {} \
+                            \nRun 'confini.exe edit --help' for edit instructions",
+                            add_matches.value_of("domain").unwrap(),
+                            d.get("username").unwrap()
+                        );
+                    }
+                }
             }
             ("edit", Some(edit_matches)) => {
-                let mut conf = Ini::load_from_file("conf.ini").unwrap();
+                let mut conf = Ini::load_from_file(FILEPATH).unwrap();
 
                 let _key = "C_KEY";
                 let _nounce = "C_NOUNCE";
@@ -168,31 +218,108 @@ fn main() {
                     &mut password[..],
                 );
 
+                let domain_config = match conf.section_mut(edit_matches.value_of("domain")) {
+                    Some(s) => s,
+                    None => {
+                        eprintln!(
+                            "Configuration not found for domain: {}",
+                            edit_matches.value_of("domain").unwrap()
+                        );
+                        std::process::exit(2)
+                    }
+                };
+
                 if edit_matches.is_present("username") {
-                    conf.with_section(edit_matches.value_of("domain"))
-                        .set("username", edit_matches.value_of("username").unwrap())
-                        .set("password", encode(&password).as_str());
+                    domain_config.insert(
+                        "username", 
+                        edit_matches.value_of("username").unwrap());
+                    domain_config.insert("password", encode(&password).as_str());
                 } else {
-                    conf.with_section(edit_matches.value_of("domain"))
-                        .set("password", encode(&password).as_str());
+                    domain_config.insert("password", encode(&password).as_str());
                 }
-                conf.write_to_file("conf.ini").unwrap();
+                conf.write_to_file(FILEPATH).unwrap();
                 println!(
-                    "Password configuration for {} has been updated",
+                    "Configuration for {} has been updated",
                     edit_matches.value_of("domain").unwrap()
                 );
             }
             ("delete", Some(delete_matches)) => {
-                let mut conf = Ini::load_from_file("conf.ini").unwrap();
+                let mut conf = Ini::load_from_file(FILEPATH).unwrap();
                 conf.delete(delete_matches.value_of("domain"));
-                conf.write_to_file("conf.ini").unwrap();
+                conf.write_to_file(FILEPATH).unwrap();
                 println!(
-                    "Password configuration for {} has been deleted",
+                    "Configuration for {} has been deleted",
                     delete_matches.value_of("domain").unwrap()
                 );
             }
-            ("", None) => println!("No subcommand was used"), // If no subcommand was usd it'll match the tuple ("", None)
-            _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
+            ("retrieve", Some(retrieve_matches)) => {
+                let conf = Ini::load_from_file(FILEPATH).unwrap();
+                let _key = match env::var("C_KEY") {
+                    Ok(k) => k,
+                    Err(e) => {
+                        println!("Essential ENV variables could not be read\n \
+                        Try running the shell with elevated priviliges or \
+                        restarting the shell.");
+                        eprintln!("Error: C_KEY {:?}", e);
+                        std::process::exit(1)
+                    }
+                };
+                let _nounce = match env::var("C_NOUNCE") {
+                    Ok(k) => k,
+                    Err(e) => {
+                        println!("Essential ENV variables could not be read\n \
+                        Try running the shell with elevated priviliges or \
+                        restarting the shell.");
+                        eprintln!("Error: C_NOUNCE {:?}", e);
+                        std::process::exit(1)
+                    }
+                };
+                let key = decode(_key).unwrap();
+                let nounce = decode(_nounce).unwrap();
+                let mut decryptor = aes::ctr(KeySize::KeySize128, &key, &nounce);
+                let mut final_result = Vec::<u8>::new();
+
+                let domain_config = match conf.section(retrieve_matches.value_of("domain")) {
+                    Some(s) => s,
+                    None => {
+                        eprintln!(
+                            "Configuration not found for domain: {}",
+                            retrieve_matches.value_of("domain").unwrap()
+                        );
+                        std::process::exit(2)
+                    }
+                };
+                let encrypted_data = decode(domain_config.get("password").unwrap()).unwrap();
+                let mut read_buffer = buffer::RefReadBuffer::new(&encrypted_data);
+                let mut buffer = [0; 4096];
+                let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+
+                loop {
+                    let result = decryptor
+                        .decrypt(&mut read_buffer, &mut write_buffer, true)
+                        .unwrap();
+                    final_result.extend(
+                        write_buffer
+                            .take_read_buffer()
+                            .take_remaining()
+                            .iter()
+                            .map(|&i| i),
+                    );
+                    match result {
+                        BufferResult::BufferUnderflow => break,
+                        BufferResult::BufferOverflow => {}
+                    }
+                }
+                println!(
+                    "Configuration for {}:\nUsername: {}\nPassword: {}",
+                    retrieve_matches.value_of("domain").unwrap(),
+                    domain_config.get("username").unwrap(),
+                    std::str::from_utf8(&final_result).unwrap()
+                );
+            }
+            ("", None) => println!("No subcommand was used"),
+            _ => println!("Usage Error: Run 'confini.exe --help' for usage \
+                instructions."),
         }
     }
 }
